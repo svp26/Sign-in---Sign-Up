2cb424f78a7e491ec91462246a57ce530d7e1e78
svp26 <purnimah.s.v@gmail.com>
2023-09-15 07:48:34 +0530

    first commit

---
+++ dist/index.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var n = 2;
+
+function sqaure(num) {
+  var ans = num * num;
+  return ans;
+}
+
+var sqaure2 = sqaure(n);
+var sqaure4 = sqaure(4);
---
+++ index.js
@@ -0,0 +1,10 @@
+var n=2
+
+function sqaure(num){
+
+    var ans=num*num;
+    return ans;
+}
+
+var sqaure2=sqaure(n);
+var sqaure4=sqaure(4);
